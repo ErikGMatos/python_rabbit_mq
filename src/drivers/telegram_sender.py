bfbdb8e3fd2e05b33c29c63474270c3d9374335f
Erik Garces Matos <erik_senac@hotmail.com>
2024-12-20 21:20:59 -0300

    Update telegram_sender.py

--- src/drivers/telegram_sender.py
+++ src/drivers/telegram_sender.py
@@ -2,8 +2,8 @@ import requests
 
 
 def send_telegram_message(message):
-    token = "7560737345:AAFW2eZy_eIU1nJ5Wok6epjr8KuQPR57MlQ"
-    chat_id = -1002276510391
+    token = "<token_telegram>"
+    chat_id = 0 # chat-id telegram
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {"chat_id": chat_id, "text": message}
     requests.post(url, data=payload, timeout=10)
